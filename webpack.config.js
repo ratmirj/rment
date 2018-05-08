697d03eb78d533a5483ba35dd5e06eaf6d5741ca
Ratmir Junusow <ratmir.junusow@gmail.com>
2018-05-08 11:56:31 +0200

    Separated production and development config into separate files. Added folder with build artifacts to .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,124 +1,14 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
+const producitonConfig = require('./webpack_conf/webpack.production.config');
+const developmentConfig = require('./webpack_conf/webpack.development.config');
 
 
 
 module.exports = env => {
     if(!env.production){
-        console.log("Development mode");
-        return{
-            context: path.join(__dirname, './client'),
-            entry: [
-                './src/index.js'
-            ],
-            output: {
-                path: path.join(__dirname, './client/dist'),
-                filename: 'js/index.js',
-                publicPath: '/'
-            },
-            resolve: {
-                extensions: ['.js']
-            },
-            devServer: {
-                hot: true,
-                contentBase: './client/dist',
-                historyApiFallback: true,
-                port: 8080
-            },
-            module: {
-                rules: [{
-                    test: /\.js$/,
-                    exclude: ["/node_modules/", "/first-task/"],
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['env', 'react']
-                        }
-                    }
-                },
-                    {
-                        test: /\.scss$/,
-                        use: ['style-loader', 'css-loader', 'sass-loader']
-                    },
-                    {
-                        test: /\.html$/,
-                        use: ['html-loader']
-                    }
-                ]
-            },
-
-            plugins: [
-                new webpack.HotModuleReplacementPlugin(),
-                new webpack.NamedModulesPlugin(),
-                new HtmlWebpackPlugin({
-                    template: './src/index.html',
-                    filename: 'index.html',
-                    inject: 'body'
-                }),
-            ]
-        }
-
+        return developmentConfig(__dirname);
     }
     else{
-        console.log("Producton mode");
-        return{
-            context: path.join(__dirname, './client'),
-            entry: [
-                './src/index.js'
-            ],
-            output: {
-                path: path.join(__dirname, './client/dist'),
-                filename: 'js/index.js',
-                publicPath: '/'
-            },
-            resolve: {
-                extensions: ['.js']
-            },
-            devServer: {
-                hot: true,
-                contentBase: './client/dist',
-                historyApiFallback: true,
-                port: 8080
-            },
-            module: {
-                rules: [{
-                    test: /\.js$/,
-                    exclude: ["/node_modules/", "/first-task/"],
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['env', 'react']
-                        }
-                    }
-                },
-                    {
-                        test: /\.scss$/,
-                        use: ['style-loader', 'css-loader', 'sass-loader']
-                    },
-                    {
-                        test: /\.html$/,
-                        use: ['html-loader']
-                    }
-                ]
-            },
-
-            plugins: [
-                new webpack.HotModuleReplacementPlugin(),
-                new webpack.NamedModulesPlugin(),
-                new HtmlWebpackPlugin({
-                    template: './src/index.html',
-                    filename: 'index.html',
-                    inject: 'body'
-                }),
-                new UglifyJsPlugin({
-                    exclude: [/node_modules/]
-                })
-            ]
-        }
-
+        return producitonConfig(__dirname);
     }
 
 };
---
+++ webpack_conf/webpack.development.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function(exec_dir){
+    return {
+        context: path.join(exec_dir, './client'),
+            entry: [
+        './src/index.js'
+    ],
+        output: {
+        path: path.join(exec_dir, './client/dist'),
+            filename: 'js/index.js',
+            publicPath: '/'
+    },
+        resolve: {
+            extensions: ['.js']
+        },
+        devServer: {
+            hot: true,
+                contentBase: './client/dist',
+                historyApiFallback: true,
+                port: 8080
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: ["/node_modules/", "/first-task/"],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react']
+                    }
+                }
+            },
+                {
+                    test: /\.scss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.html$/,
+                    use: ['html-loader']
+                }
+            ]
+        },
+
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NamedModulesPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: 'index.html',
+                inject: 'body'
+            }),
+        ]
+    };
+
+
+
+};
